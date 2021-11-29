2ed853d9a3e00092b2ccf2e304ffd0f0cb3b84b5
Shahrzad Samimi <shahrzadd@gmail.com>
2021-11-28 19:46:15 -0500

    Product function defined-All tests passed successfully

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,20 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
+function product(num1, num2) {
+  if(num1 == 0 || num2 == 0) {
+    return 0;
+  }
 
+  if (num1 == null && num2 == null) {
+    return 0;
+  }
+
+  else if (num2 == null) {
+    return num1;
+  }
+
+  return num1 * num2;
 }
 
 // ==================================================
@@ -20,21 +32,29 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
+  var result = product(2, 0);
+  if (result !== 0) throw new Error('Expected product(2, 0) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = product(2, 2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2, 2) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = product(2);
+  if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = product();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
