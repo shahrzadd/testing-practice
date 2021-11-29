2ba2e0b70f34d306224ca40a660146470f9347a2
Shahrzad Samimi <shahrzadd@gmail.com>
2021-11-28 19:45:36 -0500

    Difference function defined-All tests passed successfully

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -1,10 +1,18 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function difference() {
+function difference(num1, num2) {
+  if (num1 == null && num2 == null) {
+    return 0;
 
+  } else if (num2 == null) {
+    return num1;
+  } 
+  
+  else {
+    return num1 - num2;
+  }
 }
-
 // ==================================================
 // DEFINE AND EXECUTE TEST CASES
 // ==================================================
@@ -20,16 +28,22 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = difference(1, 1, 2, 3);
+  if (result !== 0) throw new Error('Expected difference(1, 1, 2, 3) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(1);
+  if (result !== 1) throw new Error('Expected difference(1) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = difference();
+  if (result !== 0) throw new Error('Expected difference() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
