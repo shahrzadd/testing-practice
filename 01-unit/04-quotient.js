5519782482d94d73cf5f1ba681185e4c12422b00
Shahrzad Samimi <shahrzadd@gmail.com>
2021-11-28 20:02:08 -0500

    Quotient function defined-All tests passes successfully

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,8 +1,20 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient() {
+function quotient(num1, num2) {
+  if (num2 == 0) {
+    return 'ERROR';
+  }
 
+  if (num1 == null && num2 == null) {
+    return 0;
+  }
+
+  else if (num2 == null) {
+    return num1;
+  }
+
+return num1 / num2;
 }
 
 // ==================================================
@@ -20,21 +32,29 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
+  var result = quotient(2, 0);
+  if (result !== 'ERROR') throw new Error('Expected quotient(2, 0) to be ERROR. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = quotient(4, 2, 2);
+  if (result !== 2) throw new Error('Expected quotient(4, 2, 2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = quotient(2);
+  if (result !== 2) throw new Error('Expected quotient(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
